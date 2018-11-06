6d2cdc5d03040cd52714d8a5122e5c5677313008
wan <ansatgol@gmail.com>
2018-11-06 13:57:22 +0900

    Remove unused source

--- src/main/java/com/wan/todo/common/exception/ErrorResponse.java
+++ src/main/java/com/wan/todo/common/exception/ErrorResponse.java
@@ -21,7 +21,6 @@ public class ErrorResponse {
     private int status;
     private String timestamp = ZonedDateTime.now().toOffsetDateTime().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     private List<FieldError> errors;
-//    private Detail detail;
 
     @Builder
     public ErrorResponse(String message, String code, int status, List<FieldError> errors) {
@@ -29,7 +28,6 @@ public class ErrorResponse {
         this.code = code;
         this.status = status;
         this.errors = errors == null ? new ArrayList<>() : errors;
-//        this.detail = detail;
     }
 
 
@@ -47,50 +45,6 @@ public class ErrorResponse {
             this.reason = reason;
         }
     }
-
-//    @Getter
-//    @NoArgsConstructor(access = AccessLevel.PRIVATE)
-//    public static class Detail<T> {
-//        private String className;
-//        private String methodName;
-//        private long lineNumber;
-//        private String exceptionName;
-//        private String errorMessage;
-//        private String path;
-//        private String httpMethod;
-//        private T value;
-//
-//
-//        @Builder
-//        public Detail(String className, String methodName, long lineNumber, String exceptionName, String errorMessage, String path, String httpMethod, T value) {
-//            this.className = className;
-//            this.methodName = methodName;
-//            this.lineNumber = lineNumber;
-//            this.exceptionName = exceptionName;
-//            this.errorMessage = errorMessage;
-//            this.path = path;
-//            this.httpMethod = httpMethod;
-//            this.value = hasDefaultConstructor(value) ? value : (T) new ValueEmpty();
-//        }
-//
-//
-//        // 기본 생성자가 없는 경우 Deserialize 진행하지 못하니 ValueEmpty 처리
-//        private boolean hasDefaultConstructor(T value) {
-//            try {
-//                final Class<?> valueClass = value.getClass();
-//                valueClass.getDeclaredConstructor();
-//                return true;
-//            } catch (Exception e) {
-//                log.error(e.getMessage(), e);
-//            }
-//            return false;
-//        }
-//
-//        @Getter
-//        private static class ValueEmpty {
-//            private final String message = "There is no default constructor.";
-//        }
-//    }
 
 }
 
